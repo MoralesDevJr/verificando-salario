686b2fb5dfcbbf433a81a3dec29a18498b92f90a
FMX <felipemxavier@hotmail.com.br>
2025-11-26 19:25:16 -0300

    Tentativa de ADICIONAL NOTURNO

--- src/counter/nightShift/NightShift.java
+++ src/counter/nightShift/NightShift.java
@@ -1,5 +1,24 @@
 package counter.nightShift;
 
 public class NightShift {
+    private String diasTrabalhados;
+    private double salario;
 
+    public NightShift() {}
+
+    public NightShift(String diasTrabalhados, double salario) {
+        this.diasTrabalhados = diasTrabalhados;
+        this.salario = 0;
+    }
+
+    public String getDiasTrabalhados() {
+        return diasTrabalhados;
+    }
+    public void setDiasTrabalhados(String diasTrabalhados) {
+        this.diasTrabalhados = diasTrabalhados;
+    }
+
+    public double addNoturno(double horasTrabalhadas, double addNot) {
+        return horasTrabalhadas * addNot;
+    }
 }
